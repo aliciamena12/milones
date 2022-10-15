5256d5df48d2cc856c0cc55564c7eaa54a8a62e8
aliciamena <aliciamena12@gmail.com>
2022-10-16 01:14:14 +0200

    added initial structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'milones';
+export class AppComponent implements OnInit{
+
+  public title: string = 'milones';
+  public name: string = 'milones';
+  public ngOnInit() {
+    console.log(this.title);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContainerComponent } from './container/container.component';
+import { GameListComponent } from './game-list/game-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContainerComponent,
+    GameListComponent
   ],
   imports: [
     BrowserModule
