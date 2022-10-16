b62b7366333e2c7e05ba19906ec97958782e6e43
omloklk <omar.loza.martinez@hotmail.com>
2022-10-16 22:37:02 +0200

    Working on game list

--- src/app/game-list/game-list.component.ts
+++ src/app/game-list/game-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from './game.model';
 
 @Component({
   selector: 'app-game-list',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameListComponent implements OnInit {
 
-  constructor() { }
+  public games: Game[] = [
+    {title:  "a"}
+  ];
+  constructor() {
+
+    
+  }
 
   ngOnInit(): void {
+    console.log('aaaaa');
   }
 
 }
